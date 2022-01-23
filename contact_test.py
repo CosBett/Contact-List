ca6056576d55d3187b813ec39219c7d654d7e3a5
CosBett <cosmasbett9@gmail.com>
2022-01-23 16:17:46 +0300

    add setup method to define instruction for testing

--- contact_test.py
+++ contact_test.py
@@ -10,3 +10,24 @@ class TestContact(unittest.TestCase):
     Args:
         unittest.TestCase: TestCase class that helps in creating test cases
     '''
+
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_contact = Contact(
+            "Cosmas", "Bett", "0708602035", "cosmasbett9@gmail.com")  # create contact object
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_contact.first_name, "Cosmas")
+        self.assertEqual(self.new_contact.last_name, "Bett")
+        self.assertEqual(self.new_contact.phone_number, "0708602035")
+        self.assertEqual(self.new_contact.email, "cosmasbett9@gmail.com")
+
+
+if __name__ == '__main__':
+    unittest.main()
