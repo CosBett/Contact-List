dda95064146ae31e12d65dc5e20ce7e1f36075f8
CosBett <cosmasbett9@gmail.com>
2022-01-24 11:02:10 +0300

    create method to delete contact object from the content list

--- run.py
+++ run.py
@@ -15,3 +15,10 @@ def save_contacts(contact):
     Function to save contact
     '''
     contact.save_contact()
+
+
+def del_contact(contact):
+    '''
+    Function to delete a contact
+    '''
+    contact.delete_contact()
